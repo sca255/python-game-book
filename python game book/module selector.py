9645400905f0ae0fd8c7ca800205df0f8493719c
arpan das <admin@27jb48.onmicrosoft.com>
2023-07-08 22:17:56 +0530

    adding files

---
+++ python game book/module selector.py
@@ -0,0 +1,9 @@
+#from . import joke_teller
+#from . import numberGuess1
+import numberGuess1
+import joke_teller
+ipt = input("module to run?")
+if "number guess" in ipt:
+    numberGuess1
+if "joke teller" in ipt:
+    joke_teller
